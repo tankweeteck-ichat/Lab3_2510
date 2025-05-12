b00c4493806c90db87ef38a464b4585c00015879
ktsp <rommel6853@yahoo.com>
2025-05-12 20:56:58 +0800

    Implemented get_employees_by_dept()

--- employee_info.py
+++ employee_info.py
@@ -33,7 +33,9 @@ def get_employees_by_dept(department):
     result = []
 
     # Add your implementation from here
-
+    for eachRecord in employee_data:
+        if eachRecord['department'] == department:
+            result.append(eachRecord)
 
     return result
 
